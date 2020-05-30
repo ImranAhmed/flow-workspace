2eebc6c031a0282a08aa39980858b1335c48e931
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-05-30 13:33:05 +0100

    Added storybook addons

--- libs/ui-kit/.storybook/webpack.config.js
+++ libs/ui-kit/.storybook/webpack.config.js
@@ -1,7 +1,20 @@
 const rootWebpackConfig = require('../../../.storybook/webpack.config');
+const path = require('path');
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
   config = await rootWebpackConfig({ config, mode });
-  
+  config.module.rules.push({
+    test: /\.stories\.ts?$/,
+    loaders: [
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: {
+          parser: 'typescript',
+        },
+      },
+    ],
+    enforce: 'pre',
+    include: [path.resolve(__dirname, '../src')],
+  });
   return config;
 };
