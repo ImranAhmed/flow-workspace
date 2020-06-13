4bcdf594d331ef423840b6320bd80a6fddc14757
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-06-13 12:30:12 +0100

    Removed redundent methods

--- libs/ui-kit/src/lib/components/user-details/user-details.component.ts
+++ libs/ui-kit/src/lib/components/user-details/user-details.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { User } from '@flow-workspace/utilities';
 
 @Component({
@@ -7,8 +7,6 @@ import { User } from '@flow-workspace/utilities';
   styleUrls: ['./user-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent {
   @Input() user: User;
-
-  ngOnInit(): void {}
 }
